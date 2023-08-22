2bf2e34883a10c74ee5ce0b45feb01400f086cf8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-22 14:45:02 -0300

    Variaveis

---
+++ ConhecendoKotlin/src/main/kotlin/introducao/Variaveis.kt
@@ -0,0 +1,22 @@
+package introducao
+
+fun someCondition() = true
+
+fun main() {
+    var a: String = "initial"
+    println(a)
+    val b: Int = 1
+    val c = 3
+
+    val d: Int
+
+    if (someCondition()) {
+        d = 1
+    } else {
+        d = 2
+    }
+
+    println(d)
+
+    var e: Int
+}
