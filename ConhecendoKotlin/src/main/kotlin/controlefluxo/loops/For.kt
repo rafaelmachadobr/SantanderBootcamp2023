742e1c5b1d33200c2b4a461e589c68bf43be4b3f
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-23 15:50:47 -0300

    Loops: For

---
+++ ConhecendoKotlin/src/main/kotlin/controlefluxo/loops/For.kt
@@ -0,0 +1,9 @@
+package controlefluxo.loops
+
+fun main() {
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {
+        println("Yummy, it's a $cake cake!")
+    }
+}
