cee70442fdecc595626d90ce7887a6dc6876e580
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-23 15:51:04 -0300

    Loops: Iterators

---
+++ ConhecendoKotlin/src/main/kotlin/controlefluxo/loops/Iterators.kt
@@ -0,0 +1,17 @@
+package controlefluxo.loops
+
+class Animal(val name: String)
+
+class Zoo(val animals: List<Animal>) {
+    operator fun iterator(): Iterator<Animal> {
+        return animals.iterator()
+    }
+}
+
+fun main() {
+    val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
+
+    for (animal in zoo) {
+        println("Watch out, it's a ${animal.name}")
+    }
+}
