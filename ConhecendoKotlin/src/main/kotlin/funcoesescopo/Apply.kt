c5edc7d11e86b2f626f56cc3a5a516cdc46b6d15
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-28 12:28:12 -0300

    Apply

---
+++ ConhecendoKotlin/src/main/kotlin/funcoesescopo/Apply.kt
@@ -0,0 +1,15 @@
+package funcoesescopo
+
+data class Person(var name: String, var age: Int, var about: String) {
+    constructor() : this("", 0, "")
+}
+
+fun main() {
+    val jake = Person()
+    val stringDescription = jake.apply {
+        name = "Jake"
+        age = 30
+        about = "Android developer"
+    }.toString()
+    println(stringDescription)
+}
