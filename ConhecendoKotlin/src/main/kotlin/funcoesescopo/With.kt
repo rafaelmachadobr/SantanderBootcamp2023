23502b4be535e1af7dad6ee85c16ff7c49906ecb
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-28 12:28:00 -0300

    With

---
+++ ConhecendoKotlin/src/main/kotlin/funcoesescopo/With.kt
@@ -0,0 +1,11 @@
+package funcoesescopo
+
+class Configuration(val host: String, val port: Int)
+
+fun main() {
+    val configuration = Configuration(host = "127.0.0.1", port = 9000)
+
+    with(configuration) {
+        println("$host:$port")
+    }
+}
