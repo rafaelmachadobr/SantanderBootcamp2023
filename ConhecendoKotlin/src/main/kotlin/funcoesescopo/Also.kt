103102accf760e05d1ba4290deacb2b0ec7c5c1a
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-28 12:28:19 -0300

    Also

---
+++ ConhecendoKotlin/src/main/kotlin/funcoesescopo/Also.kt
@@ -0,0 +1,12 @@
+package funcoesescopo
+
+fun writeCreationLog(p: Person) {
+    println("A new person ${p.name} was created.")
+}
+
+fun main() {
+    val jake = Person("Jake", 30, "Android developer")
+        .also {
+            writeCreationLog(it)
+        }
+}
