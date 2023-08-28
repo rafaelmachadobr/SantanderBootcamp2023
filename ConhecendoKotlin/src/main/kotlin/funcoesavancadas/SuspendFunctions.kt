b777a870c4c8e4a8a2805176f55e2e2b82772047
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-08-28 14:49:35 -0300

    Suspend Functions

---
+++ ConhecendoKotlin/src/main/kotlin/funcoesavancadas/SuspendFunctions.kt
@@ -0,0 +1,15 @@
+package funcoesavancadas
+
+//import kotlinx.coroutines.*
+//
+//fun main() = runBlocking {
+//    doWorld()
+//}
+//
+//suspend fun doWorld() = coroutineScope {
+//    launch {
+//        delay(1000L)
+//        println("World!")
+//    }
+//    print("Hello ")
+//}
