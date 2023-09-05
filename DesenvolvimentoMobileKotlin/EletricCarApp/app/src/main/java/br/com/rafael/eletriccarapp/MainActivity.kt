976d82c1f0e067a9c3b9ef02256254f3ead800be
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-09-05 13:17:22 -0300

    Recuperando Valor EditText com Kotlin

--- DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
+++ DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package br.com.rafael.eletriccarapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var preco : EditText
+    lateinit var btnCalcular : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+        setupListeners()
+    }
+
+    fun setupViews() {
+        preco = findViewById(R.id.et_preco_kwh)
+        btnCalcular = findViewById(R.id.btn_calcular)
+    }
+
+    fun setupListeners() {
+        btnCalcular.setOnClickListener {
+            val precoDigitado = preco.text
+            Log.d("Preço digitado ->", precoDigitado.toString())
+        }
     }
 }
