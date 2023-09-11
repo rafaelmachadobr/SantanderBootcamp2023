b6099152fb92e8a170c17fc219f4ec791439d2b5
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-09-11 14:05:24 -0300

    Abrir uma nova activity

--- DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
+++ DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package br.com.rafael.eletriccarapp
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import br.com.rafael.eletriccarapp.presentation.CalculadoraAutonomiaActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var preco: EditText
@@ -19,15 +23,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupViews() {
-        preco = findViewById(R.id.et_preco_kwh)
-        kmPercorrido = findViewById(R.id.et_km_percorrido)
-        resultado = findViewById(R.id.tv_resultado)
+//        preco = findViewById(R.id.et_preco_kwh)
+//        kmPercorrido = findViewById(R.id.et_km_percorrido)
+//        resultado = findViewById(R.id.tv_resultado)
+//        btnCalcular = findViewById(R.id.btn_calcular)
         btnCalcular = findViewById(R.id.btn_calcular)
     }
 
     fun setupListeners() {
         btnCalcular.setOnClickListener {
-            calcular()
+//            calcular()
+            startActivity(Intent(this, CalculadoraAutonomiaActivity::class.java))
         }
     }
 
---
+++ DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/presentation/CalculadoraAutonomiaActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rafael.eletriccarapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.rafael.eletriccarapp.R
+
+class CalculadoraAutonomiaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calcular_autonomia)
+    }
+}
