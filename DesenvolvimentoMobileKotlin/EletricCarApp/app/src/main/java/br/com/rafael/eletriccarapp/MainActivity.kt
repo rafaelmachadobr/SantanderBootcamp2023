b596b97fc7e734b71e439e35217d41cd57a26ca3
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-09-11 15:43:43 -0300

    ListView

--- DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
+++ DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
@@ -2,14 +2,15 @@ package br.com.rafael.eletriccarapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.rafael.eletriccarapp.presentation.CalculadoraAutonomiaActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnCalcular: Button
+    lateinit var lista: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +18,18 @@ class MainActivity : AppCompatActivity() {
 
         setupView()
         setupListeners()
+        setupList()
     }
 
     fun setupView() {
         btnCalcular = findViewById(R.id.btn_calcular)
+        lista = findViewById(R.id.lv_informacoes)
+    }
+
+    fun setupList() {
+        var dados = arrayOf("Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+        lista.adapter = adapter
     }
 
     fun setupListeners() {
