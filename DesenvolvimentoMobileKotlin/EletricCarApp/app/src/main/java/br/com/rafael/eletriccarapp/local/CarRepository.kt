5c6e35c8f02f2f3d79ea214c46ca802a876db9f1
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-09-21 13:34:44 -0300

    Buscando dados com o SQLite

--- DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/local/CarRepository.kt
+++ DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/local/CarRepository.kt
@@ -2,7 +2,13 @@ package br.com.rafael.eletriccarapp.local
 
 import android.content.ContentValues
 import android.content.Context
+import android.provider.BaseColumns
 import br.com.rafael.eletriccarapp.domain.Carro
+import br.com.rafael.eletriccarapp.local.CarrosContract.CarEntry.COLUMN_NAME_BATERIA
+import br.com.rafael.eletriccarapp.local.CarrosContract.CarEntry.COLUMN_NAME_POTENCIA
+import br.com.rafael.eletriccarapp.local.CarrosContract.CarEntry.COLUMN_NAME_PRECO
+import br.com.rafael.eletriccarapp.local.CarrosContract.CarEntry.COLUMN_NAME_RECARGA
+import br.com.rafael.eletriccarapp.local.CarrosContract.CarEntry.COLUMN_NAME_URL_PHOTO
 
 class CarRepository(private val context: Context) {
     fun save(carro: Carro): Boolean {
@@ -12,11 +18,11 @@ class CarRepository(private val context: Context) {
             val db = dbHelper.writableDatabase
 
             val values = ContentValues().apply {
-                put(CarrosContract.CarEntry.COLUMN_NAME_PRECO, carro.preco)
-                put(CarrosContract.CarEntry.COLUMN_NAME_BATERIA, carro.bateria)
-                put(CarrosContract.CarEntry.COLUMN_NAME_POTENCIA, carro.potencia)
-                put(CarrosContract.CarEntry.COLUMN_NAME_RECARGA, carro.recarga)
-                put(CarrosContract.CarEntry.COLUMN_NAME_URL_PHOTO, carro.urlPhoto)
+                put(COLUMN_NAME_PRECO, carro.preco)
+                put(COLUMN_NAME_BATERIA, carro.bateria)
+                put(COLUMN_NAME_POTENCIA, carro.potencia)
+                put(COLUMN_NAME_RECARGA, carro.recarga)
+                put(COLUMN_NAME_URL_PHOTO, carro.urlPhoto)
             }
 
             val inserted = db?.insert(CarrosContract.CarEntry.TABLE_NAME, null, values)
@@ -29,5 +35,32 @@ class CarRepository(private val context: Context) {
         }
 
         return isSaved
+    }
+
+    fun findCarById(id: Int) {
+        val dbHelper = CarDbHelper(context)
+        val db = dbHelper.readableDatabase
+
+        val columns = arrayOf(
+            BaseColumns._ID,
+            COLUMN_NAME_PRECO,
+            COLUMN_NAME_BATERIA,
+            COLUMN_NAME_POTENCIA,
+            COLUMN_NAME_RECARGA,
+            COLUMN_NAME_URL_PHOTO
+        )
+
+        val filter = "${BaseColumns._ID} = ?"]
+        val filterValues = arrayOf(id.toString())
+
+        val cursor = db.query(
+            CarrosContract.CarEntry.TABLE_NAME,
+            columns,
+            filter,
+            filterValues,
+            null,
+            null,
+            null
+        )
     }
 }
