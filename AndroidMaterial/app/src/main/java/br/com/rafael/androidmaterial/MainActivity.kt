ca0a284032c3684b315a2c276088fc4aca2de836
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-09-27 17:14:36 -0300

    Bottom App Bars

--- AndroidMaterial/app/src/main/java/br/com/rafael/androidmaterial/MainActivity.kt
+++ AndroidMaterial/app/src/main/java/br/com/rafael/androidmaterial/MainActivity.kt
@@ -32,5 +32,9 @@ class MainActivity : AppCompatActivity() {
         binding.cvTopAppBar.setOnClickListener {
             startActivity(TopAppBarActivity.createIntent(this))
         }
+
+        binding.cvBottomAppBar.setOnClickListener {
+            startActivity(BottomAppBarActivity.getStartIntent(this))
+        }
     }
 }
