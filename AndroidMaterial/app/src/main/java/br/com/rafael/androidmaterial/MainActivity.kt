97e019cc7c41d723eaa6b6be82e1a53c15e2f588
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-09-27 13:53:39 -0300

    Buttons

---
+++ AndroidMaterial/app/src/main/java/br/com/rafael/androidmaterial/MainActivity.kt
@@ -0,0 +1,25 @@
+package br.com.rafael.androidmaterial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rafael.androidmaterial.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.cvButtons.setOnClickListener {
+            startActivity(ButtonsActivity.getStartIntent(this))
+        }
+    }
+}
