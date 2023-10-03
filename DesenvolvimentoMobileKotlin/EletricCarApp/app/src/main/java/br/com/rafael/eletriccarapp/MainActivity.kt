e7e82e70f98bf73cf7600d4a7092d0da1253c056
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-03 13:37:28 -0300

    Refatorando o App Eletric Car

--- DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
+++ DesenvolvimentoMobileKotlin/EletricCarApp/app/src/main/java/br/com/rafael/eletriccarapp/MainActivity.kt
@@ -1,53 +1,20 @@
 package br.com.rafael.eletriccarapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
-import br.com.rafael.eletriccarapp.data.CarFactory
-import br.com.rafael.eletriccarapp.ui.CalculadoraAutonomiaActivity
-import br.com.rafael.eletriccarapp.ui.adapter.CarAdapter
-import br.com.rafael.eletriccarapp.ui.adapter.TabsAdapter
-import com.google.android.material.tabs.TabLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import br.com.rafael.eletriccarapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tabLayout: TabLayout
-    lateinit var viewPager: ViewPager2
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        setupView()
-        setupTabs()
-    }
-
-    fun setupView() {
-        tabLayout = findViewById(R.id.tab_layout)
-        viewPager = findViewById(R.id.vp_view_pager)
-    }
-
-    fun setupTabs() {
-        val tabsAdapter = TabsAdapter(this)
-        viewPager.adapter = tabsAdapter
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                tab?.let { viewPager.currentItem = it.position }
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
-        })
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                tabLayout.getTabAt(position)?.select()
-            }
-        })
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupWithNavController(binding.bottomNavigation, navController)
     }
 }
